0d030905f3ff2c77d8604701faddfa77c42560e2
Somu Mohan Reddy <mohanreddysomu44@gmail.com>
2026-06-27 15:16:52 +0530

    Update WebConfig.java

--- medvalt/src/main/java/com/medvalt/medvalt/config/WebConfig.java
+++ medvalt/src/main/java/com/medvalt/medvalt/config/WebConfig.java
@@ -13,10 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:3000",
-                            "https://medvault-frontend-pink.vercel.app"
-                        )
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
